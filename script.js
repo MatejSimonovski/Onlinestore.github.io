709bb52a9d3b3bcb6d8a2a623b8d6bf5f448e34e
MatejSimonovski <167614953+MatejSimonovski@users.noreply.github.com>
2025-03-29 11:16:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,4 +12,12 @@ for (let i = 0; i < item_names.length ; i += 1) {
     item_name_objects[i].innerHTML = item_names[i]
 }
 
+let block = document.querySelector('.block')
+document.addEventListener('mousemove', function(e) {
+    let dx = e.pageX - window.innerWidth / 2
+    let dy = e.pageY - window.innerHeight / 2
+    let angleX = 20 * dx / window.innerWidth / 2
+    let angleY = 20 * dy / window.innerHeight / 2
+    block.style.transform = `rotateX(${-angleY}deg) rotateY(${angleX}deg)`
+})
 
